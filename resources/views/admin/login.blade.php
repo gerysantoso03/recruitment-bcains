3bb6fec8d7d934ebb35ad8dd5186fee4205cdc89
gerysantoso03 <gerysantos03@gmail.com>
2024-02-07 11:05:25 +0700

    add new layout for admin and login page

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.admin-master')
+@section('content')
+    <main class="flex ">
+        {{-- Login Form --}}
+        <div class="flex-1">
+
+        </div>
+        {{-- Login Page Ilustration --}}
+        <div class="flex-1">
+
+        </div>
+    </main>
+@endsection
